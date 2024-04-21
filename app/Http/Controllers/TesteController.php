142d78a6871ff6cd9bf8aafa28034ed42128c186
VitorRBrumatti <vitorrufino084@gmail.com>
2024-04-20 21:29:42 -0300

    funcional testes

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teste;
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name'=> 'required|max:25',
+            'email' => 'required',
+    
+           ]);
+        
+        $teste = Teste::create($validatedData);
+
+        return response()->json($teste, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\TesteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/teste', TesteController::class);
